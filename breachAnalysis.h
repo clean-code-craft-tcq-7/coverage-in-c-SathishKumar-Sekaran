f6712417b0fcc5533d0d5db48047261ff4170011
SathishKumar-Sekaran <115685997+SathishKumar-Sekaran@users.noreply.github.com>
2023-01-11 11:28:06 +0530

    Delete conflict enums

--- breachAnalysis.h
+++ breachAnalysis.h
@@ -1,14 +1,1 @@
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
-
-typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
-} BreachType;
-
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
